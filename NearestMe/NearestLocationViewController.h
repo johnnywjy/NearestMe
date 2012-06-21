bd0b05d406482ac76a2b2d9fb1ad518114f0d283
jy <johnny.wjy07@gmail.com>
2012-06-21 17:30:45 +0000

    add 2 controllers

---
+++ NearestMe/NearestLocationViewController.h
@@ -0,0 +1,13 @@
+//
+//  NearestLocationViewController.h
+//  NearestMe
+//
+//  Created by lynny on 12-6-21.
+//  Copyright (c) 2012年 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface NearestLocationViewController : UIViewController
+
+@end
